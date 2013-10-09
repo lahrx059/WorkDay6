a0cac12654722a0153cd555eed8c61a5aebe21c2
lahrx059 <lahrx059@umn.edu>
2013-10-09 15:15:00 -0500

    done-zo

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,7 +3,14 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+
+        Dim x As Integer = 1
+
+        Do While x <= 10
+            lstResult.Items.Add("Number is " & x ^ 2)
+            x += 1
+
+        Loop
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -4,7 +4,19 @@ Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim curYear As Integer
+        Dim pop As Double
+
+        curYear = 2012
+        pop = 7000000000
+
+        Do While pop > 6000000
+            pop = pop / 2
+            curYear = curYear - 50
+        Loop
+
+
+        MessageBox.Show("The year is " & curYear, "Years")
 
     End Sub
 End Class
